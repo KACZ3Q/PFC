394c54e59d9b0e49b3193150bbe3a4241bf8cdc7
KACZ3Q <115534849+KACZ3Q@users.noreply.github.com>
2023-07-22 19:16:29 +0200

    hope this works

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
     <Router>
       <Navbar />
       <Routes>
-        <Route path='/home'  element={ <Home />}/>
+        <Route path='/'  element={ <Home />}/>
         <Route path='/projects' element={<Projects/>}/>
         <Route path='/testimonials' element={<Testimonials/>}/>
         <Route path='/about-us' element={<AboutUs/>}/>
