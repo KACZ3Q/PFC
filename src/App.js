27559b5d05e435df434a2162321a82ecf18698dd
KACZ3Q <115534849+KACZ3Q@users.noreply.github.com>
2023-07-22 18:49:26 +0200

    some fixes

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import Footer from "./Components/Footer/Footer";
 function App() {
   return (
     <Router>
+      <Home/>
       <Navbar />
       <Routes>
         <Route path='/' exact element={ <Home />}/>
